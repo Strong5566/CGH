23210005eeb9cde2bb4a27fc0db517ccb791c52e
Strong <strongerh9@gmail.com>
2014-09-12 23:11:56 +0800

    get type size from ctypes

--- 3df.py
+++ 3df.py
@@ -2,9 +2,10 @@ import sys
 import struct
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import ctypes
 
-SIZEOF_INT=4
-SIZEOF_FLOAT=4
+SIZEOF_INT=ctypes.sizeof(ctypes.c_int)
+SIZEOF_FLOAT=ctypes.sizeof(ctypes.c_float)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -15,7 +16,7 @@ n=struct.unpack("i", f.read(SIZEOF_INT))[0]
 dat=[]
 for i in range(n):
 	x,y,z=struct.unpack("fff", f.read(SIZEOF_FLOAT*3))
-	if i%10!=0:
+	if i%100!=0:
 		continue
 	ax.scatter(x, y, z)
 
